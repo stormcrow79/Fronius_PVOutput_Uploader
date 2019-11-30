a149ec6f4fc9225d67817cfcffb21044d847bb47
Gavin Morris <gavin.morris@health.telstra.com>
2019-11-30 09:56:17 +0800

    test adding year folder

--- test.php
+++ test.php
@@ -1,9 +1,20 @@
 <?php
 
-echo expand_tilde("~");
-return;
+  // Define Date & Time
+  date_default_timezone_set("Australia/Perth");
+  $system_time= time();
+  $year = date('Y', time());
+  $date = date('Ymd', time());
+  $time = date('H:i', time());
 
-  $slug = expand_tilde("~/projects/solar/data/") . date("Ymd-Hi");
+  //$slug = expand_tilde("~/projects/solar/data/") . date("Ymd-Hi");
+  $dataFolder = "~/projects/solar";
+  $slug = expand_tilde($dataFolder."/data/".$year."/inverter-").date("Ymd-Hi");
+
+  echo $slug;
+
+  return;
+
   $json = file_get_contents("https://randomuser.mex/api");
   if ($json === FALSE) {
     $err = error_get_last();
